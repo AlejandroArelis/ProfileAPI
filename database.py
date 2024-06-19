a0546c1ca2e6edb681988f607d29486db95ac219
Alejandro Arelis <arasaka.code@outlook.com>
2024-06-18 18:55:34 -0600

    .env

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb+srv://profile_user:tReN6duf2i8LSwesec7Y4y5jOFuQeChu6RiStofinAfiPItlw3peWRIthU4eMi3T@micluster.wzbs7ki.mongodb.net/")
-db = client["profile"]
+from dotenv import load_dotenv
+load_dotenv()
+client = AsyncIOMotorClient(f"mongodb+srv://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_USER_PASSWORD")}@micluster.wzbs7ki.mongodb.net/")
+db = client[os.getenv("MONGODB_DB")]
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import logging
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from profile.routes import router as profile_routes
 from skill_group.routes import router as skill_group_routes
 from skill.routes import router as skill_routes
+from openai import OpenAI
+
+load_dotenv()
+
+ia = OpenAI()
 
 app = FastAPI()
 
 origins = [
     "http://localhost:4200",
-    "http://127.0.0.1:4200"
+    "http://127.0.0.1:4200",
+    "https://alejandroarelis.github.io"
 ]
 
 app.add_middleware(
@@ -27,8 +35,17 @@ app.include_router(skill_routes)
 
 
 @app.get("/")
-def hello():
-  return {'message': 'Holi'}
+async def hello():
+    response = ia.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+    return {'message': response}
 
 
 @app.exception_handler(Exception)
